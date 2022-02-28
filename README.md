6ba6f01bb5e9ad337131d20984f05ba09069d7bb
shukrullah shirzad <shukrullah.shirzad.779@gmail.com>
2022-02-28 15:06:22 +0430

    added responsive navbar with hero section

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
-# Hi there 👋
-
-I'm Shurkrullah Shirzad from Afghanistan. I have started to learn front-end web development in 2021. I'm very comfortable with HTML and CSS. I really enjoy learning and working with Javascript. I'll be learning React and Node next. You can check out my Twitter at https://twitter.com/cooolCoder.
-
-
-## Skills
-* HTML 
-* CSS 
-* JS
-
+http://findmatthew.com/ is the website I'm going to build
---
+++ index.js
@@ -0,0 +1,16 @@
+// code for scroll effect on navigation bar
+window.addEventListener("scroll",(e)=>{
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY>0)
+})
+
+// code for navigation apearance
+let closeBtn = document.querySelector('.nav-close');
+let openBtn = document.querySelector('.nav-open');
+let nav = document.querySelector('.nav-links');
+openBtn.addEventListener('click',(e)=>{
+   nav.classList.add('show');
+});
+closeBtn.addEventListener('click',(e)=>{
+    nav.classList.remove('show')
+})
