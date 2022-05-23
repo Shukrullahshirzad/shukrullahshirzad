e51bb29aa766af465a52134e242b00893fcc7f32
shukrullahshirzad <shukrullah.shirzad.779@gmail.com>
2022-05-23 14:16:08 +0430

    add filter functionality to projects section

--- js/index.js
+++ js/index.js
@@ -16,8 +16,30 @@ closeBtn.addEventListener('click',(e)=>{
 });
 
 //code for project's filter
+  // select filter buttons
+let buttons = document.querySelector('.shorting-menu').children;
 
-let all = document.querySelectorAll('.filter');
-forEach(element => {
-    console.log(element)
-});
+  // select items
+let item = document.querySelector('.items').children;
+for(let i = 0; i <buttons.length; i++){
+  buttons[i].addEventListener('click', (e)=>{
+    for(let j = 0; j<buttons.length; j++){
+      buttons[j].classList.remove('active')
+    }
+  buttons[i].classList.add('active');
+
+  let targetData = buttons[i].getAttribute('data-filter');
+  console.log(targetData);
+
+  for(let k = 0; k<item.length; k++){
+    item[k].classList.remove('show');
+    item[k].classList.add('hide');
+
+    if(item[k].getAttribute('data-item') == targetData || targetData == "all"){
+      item[k].classList.remove('hide');
+      item[k].classList.add('show');
+    }
+  }
+  });
+}
+  
